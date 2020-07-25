de742809a680cc0c7f0bab08e25a58955e52fe58
JoJate <jatojato@sina.cn>
2020-07-25 09:35:14 +0800

    Temp

--- loop.sh
+++ loop.sh
@@ -12,3 +12,20 @@ TARGETS=(
 for i in "${TARGETS[@]}"; do
     echo "got [${i}]"
 done
+
+function all_in_one() {
+    local text=("$@")
+    for i in "${text[*]}"; do
+        echo "one [${i}]"
+    done
+}
+
+function for_each() {
+    local text=$@
+    for i in ${text[*]}; do
+        echo "each [${i}]"
+    done
+}
+
+all_in_one 1 2 3 4 5
+for_each 1 2 3 4 5 "6 7" "8 9 X"
