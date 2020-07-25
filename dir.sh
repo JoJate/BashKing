f6b93a13c2dfc5fa8e5734eda3e8126a79ef429f
JoJate <jatojato@sina.cn>
2020-07-25 21:31:54 +0800

    bad things happen when you forget about that one little change you made ages ago

--- dir.sh
+++ dir.sh
@@ -8,12 +8,17 @@ DIR=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 
 echo "[${DIR}]"
 
-function get_random_word() {
-    local WORDFILE=/usr/share/dict/words
+function get_content() {
+    local WORDFILE="$1"
     local RAN=$$
     local lines=$(cat $WORDFILE | wc -l)
     local rnum=$((RAN * RAN * RANDOM % $lines + 1))
     echo "$(sed -n "$rnum p" "$WORDFILE" | awk '{print tolower($0)}')"
+}
+
+function get_random_word() {
+    local WORDFILE=/usr/share/dict/words
+    get_content "$WORDFILE"
 }
 
 for ((i = 0; i <= 16; i++)); do
