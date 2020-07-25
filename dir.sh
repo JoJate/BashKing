0502d3b00430464e779065568675632a1f3ac9ed
JoJate <jatojato@sina.cn>
2020-07-25 21:56:40 +0800

    This is my code. My code is amazing.

--- dir.sh
+++ dir.sh
@@ -13,14 +13,15 @@ function get_content() {
     local RAN=$$
     local lines=$(cat $WORDFILE | wc -l)
     local rnum=$((RAN * RAN * RANDOM % $lines + 1))
-    echo "$(sed -n "$rnum p" "$WORDFILE" | awk '{print tolower($0)}')"
+    echo "$(sed -n "$rnum p" "$WORDFILE")"
 }
 
 function get_random_word() {
-    local WORDFILE=/usr/share/dict/words
+    local WORDFILE=content.txt
     get_content "$WORDFILE"
 }
 
+cd "$DIR"
 for ((i = 0; i <= 16; i++)); do
     echo "$(get_random_word)"
 done
